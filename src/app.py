8738f82a8c4c74f7d768e8f63d3a0c38c8234ee8
Andrea <andrea.juarez@gmail.com>
2022-07-31 03:34:44 +0000

    Algunos cambios

---
+++ src/app.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+import seaborn as sns
+
+data = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/logistic-regression-project-tutorial/main/bank-marketing-campaign-data.csv',sep=";")
